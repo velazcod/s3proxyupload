9addd67c9c27d990c5f16650d66aec6efb060bf1
Daniel Velazco <velazcod@gmail.com>
2015-03-25 10:49:21 -0700

    Comments

--- s3uploader.go
+++ s3uploader.go
@@ -14,14 +14,17 @@ var AWS_S3_FOLDER = "";
 
 func uploadToS3(path string, hash string, data []byte) {
 
+  // Setup the AWS authentication
   auth := aws.Auth {
     AccessKey: AWS_S3_ACCESS_KEY,
     SecretKey: AWS_S3_SECRET_KEY,
   }
 
+  // Use the extension type to get the content type
   ext := strings.ToLower(strings.TrimLeft(filepath.Ext(path), "."))
   contType := fileTypesMap[ext]
 
+  // Setup the connection to S3 and bucket, and upload the file
   connection := s3.New(auth, aws.USEast)
   esBucket := connection.Bucket(AWS_S3_BUCKET_NAME)
   esBucket.Put(path, data, contType, s3.BucketOwnerFull)
