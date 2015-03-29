e32b52fe75cd70f53cb363aa8a99d99250e56a82
Daniel Velazco <velazcod@gmail.com>
2015-03-29 14:33:04 -0700

    Allow the version of the binary to be returned with the -v option

--- routes.go
+++ routes.go
@@ -3,9 +3,21 @@ package main
 import (
   "github.com/zenazn/goji"
   "flag"
+  "fmt"
+  "os"
 )
 
+const AppVersion = "1.0"
+
 func main() {
+
+  version := flag.Bool("v", false, "prints current version")
+  flag.Parse()
+  if *version {
+    fmt.Println(AppVersion)
+    os.Exit(0)
+  }
+
   if MEDIA_API_PORT == "" {
     fmt.Println("The MEDIA_API_PORT variable is required")
     os.Exit(0)
